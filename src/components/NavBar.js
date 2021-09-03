0e54a042974adcd280aa17f751c7423211b72a7f
ConstanzaCarnet <carnetconstanza@gmail.com>
2021-09-03 01:14:49 -0300

    navBar commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,50 @@
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar,Nav,NavDropdown,Container,Form,FormControl,Button } from 'react-bootstrap';
+
+export const NavBar = () => {
+    return (
+        <div>
+            <section className='portada'>
+                <div className='content'>
+                    <h2>La librería!</h2>
+                    <p>Lee los buenos libros primero; lo más seguro es que no alcances a leerlos todos.</p>
+                </div>
+            
+            </section>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">La librería</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Cosa1</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Cosa2</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Cosa 3</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Sobre la cosa</NavDropdown.Item>
+                        </NavDropdown>
+                      </Nav>
+                      <Nav>
+                        
+                         <Form className="d-flex">
+                         <FormControl
+                          type="search"
+                          placeholder="Search"
+                          className="mr-2"
+                          aria-label="Search"
+                          />
+                         <Button variant="outline-success">Search</Button>
+                        </Form>
+                      
+                     </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </div>
+    )
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavBar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
