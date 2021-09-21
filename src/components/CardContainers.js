874401c8e46584d5b1bd47bf8c7dda67d5c856e9
ConstanzaCarnet <carnetconstanza@gmail.com>
2021-09-20 22:04:07 -0300

    ApisOne5

--- src/components/CardContainers.js
+++ src/components/CardContainers.js
@@ -2,16 +2,17 @@ import *as React from "react";
 import Cards from './Cards'
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card } from "react-bootstrap";
+import {Spinner} from 'react-bootstrap';
 
 
 const CardContainers = () =>{
     const [libros , setLibros] = React.useState([]);
-    
+    const[load,setLoad]=React.useState(false);
 
     React.useEffect(()=>{
         const urlAll= 'https://www.etnassoft.com/api/v1/get/?category=libros_programacion&criteria=most_viewed'; 
-    
+        setLoad(true);
+
         fetch(urlAll)
         .then((resp) =>{
          if (resp.ok) {
@@ -22,10 +23,14 @@ const CardContainers = () =>{
         })
         .then((books)=> setLibros(books))
         .catch((error) => console.log('libro inexistente'))
+        .finally(()=> setLoad(false))
          },[]);
 
     return(
       <div className='divCard'>
+        {load? <Spinner animation="grow" className='spinner' style={{width: '6rem', height: '6rem'}}/> :null}
+         
+         
          {libros?.map(libro=> {
              return <article key={libro.category_id}>
                         <Cards 
